f01fbedc9efd83c825860deb2637c1c9a3a91982
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2022-11-22 16:53:24 -0500

    Added run_concat-ref.sh
    Added run_concat-ref.sh example for making concat'd ref genomes

---
+++ scripts/run_concat-ref.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export mm10=/shared/biodata/ngs/Reference/iGenomes/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa
+export hg38=/shared/biodata/ngs/Reference/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/BWAIndex/genome.fa
+export OUT=/fh/fast/henikoff_s/user/jgreene/refs/hg38_plus_mm10_BWA/GRCh38_plus_UCSC_mm10.fa
+
+
+#concat and index
+./concatenate_reference.py --humanRef $hg38 --mouseRef $mm10 --concatRef $OUT --tag UCSC_mm10
+bwa index $OUT
