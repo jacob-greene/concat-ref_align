d5cd0bb0a261976f86cdb77fcc4420d5b35b9428
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2022-11-23 10:20:26 -0500

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 #before running snakemake, do in tmux terminal:
 ml snakemake/5.19.2-foss-2019b-Python-3.7.4
-ml BWA/0.7.17-foss-2018b
-ml SAMtools/1.10-GCCcore-8.3.0
-ml picard/2.18.29-Java
+ml BWA/0.7.17-GCCcore-11.2.0
+ml SAMtools/1.16.1-GCC-11.2.0
+ml picard/2.25.1-Java-11
 ml Java/11.0.2
 ml GATK/4.1.4.1-GCCcore-8.3.0-Java-11
-ml R/3.6.2-foss-2019b-fh1
-
 
 #command to run snakemake (remove -np at end when done validating):
 snakemake -s subtract_mouse_and_realign.snakefile --latency-wait 60 \
