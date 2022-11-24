79f7a3e2565a0ccfb4770d4223f6357c9f960941
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2022-11-24 13:56:47 -0500

    fixed make_bed.sh

--- scripts/make_bed.sh
+++ scripts/make_bed.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ml BEDTools
-
-samtools view -B -h "${1}" \
-| bedtools bamtobed -bedpe -i stdin \
-| cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n \
-| awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' > ${2}
+samtools view "${1}" \
+    | perl -nle '@reads=split(/\t/,$_); {$start=$reads[3]-1; $end=$reads[3]+length($reads[9])-1; print "$reads[2]\t","$start\t","$end\t",$reads[0];}' \
+    | bgzip > "${2}"
